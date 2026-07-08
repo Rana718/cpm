fcbfdae8acd1777ee1fe37f055a8a6ea2aaa1f58
Rana718 <ranadolui718@gmail.com>
2026-07-09 00:52:53 +0530

    work small libary

--- testdir/main.cpp
+++ testdir/main.cpp
@@ -1,30 +1,12 @@
-#include <uWebSockets/App.h>
+#include <seastar/core/app-template.hh>
+#include <seastar/core/reactor.hh>
 #include <iostream>
 
-int main() {
-    uWS::App()
-        .get("/", [](auto *res, auto *req) {
-            res->end("hello");
-        })
-        .post("/", [](auto *res, auto *req) {
-            // Read the body and echo it back
-            std::string buffer;
-            res->onData([res, buffer = std::move(buffer)](std::string_view data, bool last) mutable {
-                buffer.append(data);
-                if (last) {
-                    res->end(buffer);
-                }
-            });
-            res->onAborted([]() {});
-        })
-        .listen(3000, [](auto *listen_socket) {
-            if (listen_socket) {
-                std::cout << "Server running on http://localhost:3000" << std::endl;
-            } else {
-                std::cerr << "Failed to listen on port 3000" << std::endl;
-            }
-        })
-        .run();
-
+int main(int argc, char** argv) {
+    seastar::app_template app;
+    app.run(argc, argv, [] {
+        std::cout << "Hello from Seastar!" << std::endl;
+        return seastar::make_ready_future<>();
+    });
     return 0;
 }
